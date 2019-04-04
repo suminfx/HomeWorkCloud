172b4317a780ae340a495dd1108c8d536444b8c8
suminfx <sumin931@yandex.ru>
2019-04-04 14:51:50 +0300

    Add Firestore Cloud

--- app/src/main/java/com/sumin/homeworkcloud/AddUserActivity.java
+++ app/src/main/java/com/sumin/homeworkcloud/AddUserActivity.java
@@ -1,13 +1,22 @@
 package com.sumin.homeworkcloud;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddUserActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     private EditText editTextName;
     private EditText editTextLastName;
@@ -18,6 +27,7 @@ public class AddUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+        db = FirebaseFirestore.getInstance();
         editTextAge = findViewById(R.id.editTextAge);
         editTextName = findViewById(R.id.editTextName);
         editTextLastName = findViewById(R.id.editTextLastName);
@@ -39,7 +49,21 @@ public class AddUserActivity extends AppCompatActivity {
 
     private void saveUser(String name, String lastName, int age, String sex) {
         if (!name.isEmpty() && !lastName.isEmpty() && !sex.isEmpty() && age > 0) {
-            Log.i("SexTest", sex);
+            db.collection("users").add(new User(name, lastName, age, sex)).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    //Тут два варианта, можно запустить новый intent в первую активность, а можно
+                    //просто у текущей активности вызвать finish() и она закроется.
+                    //Этот вариант предпочтительнее, поскольку в первом случае пользователь может
+                    //нажать кнопку back и вернется на страницу добавления. А в случае с finish() этого не произойдет
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AddUserActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/sumin/homeworkcloud/ListOfUsersActivity.java
+++ app/src/main/java/com/sumin/homeworkcloud/ListOfUsersActivity.java
@@ -8,7 +8,18 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 public class ListOfUsersActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     private FloatingActionButton fabAddUser;
     private RecyclerView recyclerViewUsers;
@@ -18,6 +29,7 @@ public class ListOfUsersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_users);
+        db = FirebaseFirestore.getInstance();
         fabAddUser = findViewById(R.id.fabAddUser);
         recyclerViewUsers = findViewById(R.id.recyclerViewUsers);
         adapter = new UsersAdapter();
@@ -28,6 +40,20 @@ public class ListOfUsersActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ListOfUsersActivity.this, AddUserActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        db.collection("users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (queryDocumentSnapshots != null) {
+                    List<User> users = queryDocumentSnapshots.toObjects(User.class);
+                    adapter.setUsers(users);
+                }
             }
         });
     }
